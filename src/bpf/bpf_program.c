8288da534378f08b41c5c62425e61f6e2426ed52
willfindlay <william@williamfindlay.com>
2019-12-24 17:17:18 -0500

    adding representation of task

--- src/bpf/bpf_program.c
+++ src/bpf/bpf_program.c
@@ -1,46 +1,7 @@
 /* https://github.com/torvalds/linux/tree/master/include/trace/events */
 
-#include <linux/sched.h>
-#include <linux/mm_types.h>
 #include "src/bpf/bpf_program.h"
-
-static inline struct pt_regs *bpf_get_current_pt_regs()
-{
-    struct task_struct* __current = (struct task_struct*)bpf_get_current_task();
-    void* __current_stack_page = __current->stack;
-    void* __ptr = __current_stack_page + THREAD_SIZE - TOP_OF_KERNEL_STACK_PADDING;
-    return ((struct pt_regs *)__ptr) - 1;
-}
-
-static inline u32 bpf_strlen(char *s)
-{
-    u32 i;
-    for (i = 0; s[i] != '\0' && i < (1 << (32 - 1)); i++);
-    return i;
-}
-
-static inline int bpf_strncmp(char *s1, char *s2, u32 n)
-{
-    int mismatch = 0;
-    for (int i = 0; i < n && i < sizeof(s1) && i < sizeof(s2); i++)
-    {
-        if (s1[i] != s2[i])
-            return s1[i] - s2[i];
-
-        if (s1[i] == s2[i] == '\0')
-            return 0;
-    }
-
-    return 0;
-}
-
-static inline int bpf_strcmp(char *s1, char *s2)
-{
-    u32 s1_size = sizeof(s1);
-    u32 s2_size = sizeof(s2);
-
-    return bpf_strncmp(s1, s2, s1_size < s2_size ? s1_size : s2_size);
-}
+#include "src/bpf/helpers.h"
 
 /* Store intermediate values between entry and exit points */
 static int store_intermediate_values(void)
@@ -59,6 +20,11 @@ int kprobe__expand_stack(struct pt_regs *ctx, struct vm_area_struct *vma, unsign
         return 0;
 
     struct mm_struct *mm = (struct mm_struct *) t->mm;
+
+    //struct rbc_task task;
+    //__builtin_memset(&task, 0, sizeof(struct rbc_task));
+    //task->pid = bpf_get_current_pid_tgid();
+    //task->tgid = (bpf_get_current_pid_tgid() >> 32);
 
     bpf_trace_printk("Expanding stack...\n");
     bpf_trace_printk("stack start: %x\n", mm->start_stack);
@@ -91,6 +57,7 @@ int kretprobe__expand_stack(struct pt_regs *ctx)
     bpf_trace_printk("stack end: %x\n", mm->start_stack + mm->stack_vm * PAGE_SIZE);
     bpf_trace_printk("stack size: %lu bytes\n", mm->stack_vm * PAGE_SIZE);
     bpf_trace_printk("Expanded stack!\n");
+    bpf_trace_printk("--------------------------\n");
 
 
     return 0;
