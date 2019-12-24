a4669ac15600cb740dc32418bed54b183566027d
willfindlay <william@williamfindlay.com>
2019-12-24 18:23:45 -0500

    wahoo, it's working

--- src/bpf/bpf_program.c
+++ src/bpf/bpf_program.c
@@ -3,90 +3,110 @@
 #include "src/bpf/bpf_program.h"
 #include "src/bpf/helpers.h"
 
-/* Store intermediate values between entry and exit points */
-static int store_intermediate_values(void)
+/* Initialization arrays for structs */
+BPF_ARRAY(__rbc_task_init, struct rbc_task, 1);
+
+/* Hashmaps */
+BPF_HASH(rbc_tasks, u64, struct rbc_task); /* pid_tgid to rbc_task */
+
+static inline struct rbc_task *update_rbc_task()
 {
-    return 0;
+    int zero = 0;
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    struct task_struct *t = (struct task_struct *)bpf_get_current_task();
+
+    /* Lookup or init rbc_task */
+    struct rbc_task *rbc_task = __rbc_task_init.lookup(&zero);
+    if (!rbc_task)
+    {
+#ifdef RBC_DEBUG
+        bpf_trace_printk("ERROR: Could not fetch rbc_task from its init array.\n");
+#endif
+        return NULL;
+    }
+    rbc_task = rbc_tasks.lookup_or_try_init(&pid_tgid, rbc_task);
+    if (!rbc_task)
+    {
+#ifdef RBC_DEBUG
+        bpf_trace_printk("ERROR: Could not lookup or init rbc_task.\n");
+#endif
+        return NULL;
+    }
+
+    /* Get current memory descriptor */
+    struct mm_struct *mm = (struct mm_struct *) t->mm;
+
+    rbc_task->pid = (pid_tgid >> 32);
+    rbc_task->tgid = pid_tgid;
+    rbc_task->stack_start = mm->start_stack;
+    rbc_task->stack_end = mm->start_stack - mm->stack_vm * PAGE_SIZE; /* TODO: change this to + based on STACKGROWSUP */
+    //rbc_task->heap_start = mm->mmap_base;
+    //rbc_task->heap_end = mm->task_size;
+    bpf_get_current_comm(rbc_task->comm, TASK_COMM_LEN);
+
+    /* Maybe print debug info */
+#ifdef RBC_DEBUG
+    bpf_trace_printk("-------------------------------\n");
+    bpf_trace_printk("Program:     %s\n", rbc_task->comm);
+    bpf_trace_printk("PID:         %u\n", rbc_task->pid);
+    bpf_trace_printk("TID:         %u\n", rbc_task->tgid);
+    bpf_trace_printk("Stack start: %x\n", rbc_task->stack_start);
+    bpf_trace_printk("Stack end:   %x\n", rbc_task->stack_end);
+    bpf_trace_printk("Stack size:  %lu bytes\n", rbc_task->stack_start - rbc_task->stack_end);
+    //bpf_trace_printk("Heap start:  %x\n", rbc_task->heap_start);
+    //bpf_trace_printk("Heap end:    %x\n", rbc_task->heap_end);
+    bpf_trace_printk("-------------------------------\n");
+#endif
+
+    return rbc_task;
 }
+
+/* BPF programs below this line ---------------------------------- */
 
 int kprobe__expand_stack(struct pt_regs *ctx, struct vm_area_struct *vma, unsigned long addr)
 {
-    struct task_struct *t = (struct task_struct *)bpf_get_current_task();
+#ifdef RBC_COMM
     char comm[TASK_COMM_LEN];
     bpf_get_current_comm(comm, TASK_COMM_LEN);
-
     /* Check if we are in the specified comm */
-    if (bpf_strncmp(comm, EXECUTABLE, TASK_COMM_LEN))
+    if (bpf_strncmp(comm, RBC_COMM, TASK_COMM_LEN))
         return 0;
+#endif
 
-    struct mm_struct *mm = (struct mm_struct *) t->mm;
-
-    //struct rbc_task task;
-    //__builtin_memset(&task, 0, sizeof(struct rbc_task));
-    //task->pid = bpf_get_current_pid_tgid();
-    //task->tgid = (bpf_get_current_pid_tgid() >> 32);
-
+#ifdef RBC_DEBUG
     bpf_trace_printk("Expanding stack...\n");
-    bpf_trace_printk("stack start: %x\n", mm->start_stack);
-    bpf_trace_printk("stack end: %x\n", mm->start_stack + mm->stack_vm);
-    bpf_trace_printk("stack size: %lu bytes\n", mm->stack_vm * PAGE_SIZE);
-    bpf_trace_printk("--------------------------\n");
+#endif
+
+    struct rbc_task *r = update_rbc_task();
+
     return 0;
 }
 
 int kretprobe__expand_stack(struct pt_regs *ctx)
 {
-    struct task_struct *t = (struct task_struct *)bpf_get_current_task();
+#ifdef RBC_COMM
     char comm[TASK_COMM_LEN];
     bpf_get_current_comm(comm, TASK_COMM_LEN);
-
     /* Check if we are in the specified comm */
-    if (bpf_strncmp(comm, EXECUTABLE, TASK_COMM_LEN))
+    if (bpf_strncmp(comm, RBC_COMM, TASK_COMM_LEN))
         return 0;
+#endif
 
-    struct mm_struct *mm = (struct mm_struct *) t->mm;
-    unsigned long start_stack = mm->start_stack;
-
+    /* Check if there was an error expanding the stack */
     if(PT_REGS_RC(ctx))
     {
+#ifdef RBC_DEBUG
         bpf_trace_printk("Couldn't expand stack!\n");
-        return 0;
+#endif
+    }
+    else
+    {
+#ifdef RBC_DEBUG
+    bpf_trace_printk("Expanded stack!\n");
+#endif
     }
 
-    bpf_trace_printk("stack start: %x\n", mm->start_stack);
-    bpf_trace_printk("stack end: %x\n", mm->start_stack + mm->stack_vm * PAGE_SIZE);
-    bpf_trace_printk("stack size: %lu bytes\n", mm->stack_vm * PAGE_SIZE);
-    bpf_trace_printk("Expanded stack!\n");
-    bpf_trace_printk("--------------------------\n");
-
-
-    return 0;
-}
-
-int kprobe__vm_brk(struct pt_regs *ctx)
-{
-    struct task_struct *t = (struct task_struct *)bpf_get_current_task();
-    char comm[TASK_COMM_LEN];
-    bpf_get_current_comm(comm, TASK_COMM_LEN);
-
-    /* Check if we are in the specified comm */
-    if (bpf_strncmp(comm, EXECUTABLE, TASK_COMM_LEN))
-        return 0;
-
-    struct mm_struct *mm = (struct mm_struct *) t->mm;
-    unsigned long start_stack = mm->start_stack;
-
-    //if(PT_REGS_RC(ctx))
-    //{
-    //    bpf_trace_printk("Couldn't expand heap!\n");
-    //    return 0;
-    //}
-
-    bpf_trace_printk("heap start: %x\n", mm->start_brk);
-    bpf_trace_printk("heap end: %x\n", mm->brk);
-    bpf_trace_printk("heap size: %lu bytes\n", mm->brk - mm->start_brk);
-    bpf_trace_printk("Expanded heap!\n");
-
+    struct rbc_task *r = update_rbc_task();
 
     return 0;
 }
--- src/bpf/bpf_program.h
+++ src/bpf/bpf_program.h
@@ -6,13 +6,13 @@
 
 struct rbc_task
 {
-    char comm[TASK_COMM_LEN];
     u32 pid;
     u32 tgid;
     unsigned long stack_start;
     unsigned long stack_end;
     unsigned long heap_start;
     unsigned long heap_end;
+    char comm[TASK_COMM_LEN];
 };
 
 #endif /* BPF_PROGRAM_H */
